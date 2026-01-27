cd8dc01e71edd7d1c53bfa321581f2607f365e2e
F4NT1N <fantin.pesant@gmail.com>
2026-01-27 11:50:19 +0100

    fix #1.3 Mapping Project et Enterprise

--- src/main/java/ourbusinessproject/Enterprise.java
+++ src/main/java/ourbusinessproject/Enterprise.java
@@ -1,10 +1,18 @@
 package ourbusinessproject;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 
+@Entity
 public class Enterprise {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
     @NotBlank
     private String name;
     @Size(min=10)
@@ -14,36 +22,58 @@ public class Enterprise {
     @NotBlank @Email
     private String contactEmail;
 
-    public Enterprise(){}
-
+    /**
+     * @param name
+     */
     public void setName(String name) {
         this.name = name;
     }
 
+    /**
+     * @return
+     */
     public String getName() {
         return this.name;
     }
 
+    /**
+     * @param description
+     */
     public void setDescription(String description) {
         this.description = description;
     }
 
+    /**
+     * @return
+     */
     public String getDescription() {
         return this.description;
     }
 
+    /**
+     * @param contactName
+     */
     public void setContactName(String contactName) {
         this.contactName = contactName;
     }
 
+    /**
+     * @return
+     */
     public String getContactName() {
         return this.contactName;
     }
 
+    /**
+     * @param contactEmail
+     */
     public void setContactEmail(String contactEmail) {
         this.contactEmail = contactEmail;
     }
 
+    /**
+     * @return
+     */
     public String getContactEmail() {
         return this.contactEmail;
     }
--- src/main/java/ourbusinessproject/Project.java
+++ src/main/java/ourbusinessproject/Project.java
@@ -1,26 +1,43 @@
 package ourbusinessproject;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import jakarta.validation.constraints.NotBlank;
 
+@Entity
 public class Project {
+    @Id @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
     @NotBlank
     private String title;
     private String description;
 
-    public Project() {}
-
+    /**
+     * @param title
+     */
     public void setTitle(String title) {
         this.title = title;
     }
 
+    /**
+     * @return
+     */
     public  String getTitle() {
         return this.title;
     }
 
+    /**
+     * @param description
+     */
     public void setDescription(String description) {
         this.description = description;
     }
 
+    /**
+     * @return
+     */
     public String getDescription() {
         return this.description;
     }
