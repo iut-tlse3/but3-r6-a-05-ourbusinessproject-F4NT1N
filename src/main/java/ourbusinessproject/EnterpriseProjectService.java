6646b9062b10873522bb83bcc38120e50289fbd6
F4NT1N <fantin.pesant@gmail.com>
2026-01-30 09:58:50 +0100

    fix #3.5 optimization SQL

--- src/main/java/ourbusinessproject/EnterpriseProjectService.java
+++ src/main/java/ourbusinessproject/EnterpriseProjectService.java
@@ -85,7 +85,7 @@ public class EnterpriseProjectService {
      * @return The list of all project sorted by title
      */
     public List<Project> findAllProjects() {
-        String query = "SELECT p FROM Project p ORDER BY p.title";
+        String query = "SELECT p FROM Project p JOIN FETCH p.enterprise ORDER BY p.title";
         TypedQuery<Project> queryObj = entityManager.createQuery(query, Project.class);
         return queryObj.getResultList();
     }
